6c223d33e9b7d574592bfecb64e2f11fa48c0fb6
proper-work <properworkstudio@gmail.com>
2019-09-13 16:54:41 +0800

    Updates

---
+++ precache-manifest.76db52626df4ed629513a6fc233564e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abd09c23cdb58f991a358a267990e9ae",
+    "url": "/ntmu_autumn_2019/index.html"
+  },
+  {
+    "revision": "d3eee72fbefb426c3e55",
+    "url": "/ntmu_autumn_2019/static/css/main.0d38c0a0.chunk.css"
+  },
+  {
+    "revision": "aab1579094eabc62a0b8",
+    "url": "/ntmu_autumn_2019/static/js/2.aabaeeee.chunk.js"
+  },
+  {
+    "revision": "d3eee72fbefb426c3e55",
+    "url": "/ntmu_autumn_2019/static/js/main.600b2f61.chunk.js"
+  },
+  {
+    "revision": "9ff1a96a71e1d14f4a45",
+    "url": "/ntmu_autumn_2019/static/js/runtime~main.e1333e3c.js"
+  }
+]);
